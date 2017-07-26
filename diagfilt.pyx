74cc2d8ff070adad37a8197c752e6fd480060b70
polaznik <polaznik>
2017-07-27 00:04:02 +0200

    primena filtera i na ivice spektra

--- diagfilt.pyx
+++ diagfilt.pyx
@@ -5,58 +5,59 @@ cimport cython
 DTYPE = np.float64
 ctypedef np.float64_t DTYPE_t
 
-
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cdef DTYPE_t median(np.ndarray[DTYPE_t, ndim=1] x):
+cdef DTYPE_t median(np.ndarray[DTYPE_t, ndim=1] x, int start, int end):
     cdef DTYPE_t temp
-    cdef long int i, j
-    cdef long int n = x.shape[0]
+    cdef int i, j
+    cdef int n = end - start
+    cdef int mid = start + n // 2
 
-    '''for i in range(n):
-        for j in range(i+1, n):
-            if(x[j] < x[i]):
-                temp = x[i]
-                x[i] = x[j]
-                x[j] = temp'''
-
-    i = 1
-    for i in range(n):
+    for i in range(start + 1, end):
         temp = x[i]
         j = i - 1
-        while j >= 0 and x[j] > temp:
-            x[j+1] = x[j]
+        while j >= start and x[j] > temp:
+            x[j + 1] = x[j]
             j = j - 1
-        x[j+1] = temp
+        x[j + 1] = temp
 
-    if(n%2==0):
-        return((x[n//2] + x[n//2 - 1]) / 2.0)
+    if (n % 2 == 0):
+        return ((x[mid - 1] + x[mid]) / 2.0)
     else:
-        return x[n//2]
+        return x[mid]
 
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
+#@cython.boundscheck(False)
+#@cython.wraparound(False)
 def diagfilter(np.ndarray[DTYPE_t, ndim=2] spect, int steph, int stepw, int count):
     spect = np.copy(spect)
     cdef int m = spect.shape[0]
     cdef int n = spect.shape[1]
-    cdef np.ndarray[DTYPE_t, ndim=1] values = np.zeros(count, dtype=DTYPE) # vrednosti od kojih se uzima medijana
+    cdef np.ndarray[DTYPE_t, ndim=1] values = np.zeros(count, dtype=DTYPE)  # vrednosti od kojih se uzima medijana
     cdef int i, j, k
-    cdef int half = (count-1)//2
-    cdef int w = half*stepw
-    cdef int h = half*steph
+    cdef int half = (count - 1) // 2
+    cdef int w = half * stepw
+    cdef int h = half * steph
     if h < 0:
         h = -h
 
-    for i in range(h, m-h):
-        for j in range(w, n-w):
-            for k in range(-half, half+1):
-                values[k+half]=spect[i+k*steph, j+k*stepw]
-            spect[i, j] = median(values)
+    #TODO
+    for i in range(h, m - h):
+        for j in range(0, w):
+            for k in range(-(j // stepw), half + 1):
+                values[k + half] = spect[i + k * steph, j + k * stepw]
+            spect[i, j] = median(values, half-(j//stepw), count)
+        for j in range(w, n - w):
+            for k in range(-half, half + 1):
+                values[k + half] = spect[i + k * steph, j + k * stepw]
+            spect[i, j] = median(values, 0, count)
+        for j in range(n-w, n):
+            for k in range(-half, (n-j)//stepw):
+                values[k + half] = spect[i + k * steph, j + k * stepw]
+            spect[i, j] = median(values, 0,  half + (n - j)//stepw)
+
 
     return spect
-
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -64,17 +65,25 @@ def vertfilter(np.ndarray[DTYPE_t, ndim=2] spect, int count):
     spect = np.copy(spect)
     cdef int m = spect.shape[0]
     cdef int n = spect.shape[1]
-    cdef np.ndarray[DTYPE_t, ndim=1] values = np.zeros(count, dtype=DTYPE) # vrednosti od kojih se uzima medijana
+    cdef np.ndarray[DTYPE_t, ndim=1] values = np.zeros(count, dtype=DTYPE)  # vrednosti od kojih se uzima medijana
     cdef int i, j, k
-    cdef int half = (count-1)//2
-    for i in range(half, m-half):
-        for j in range(n):
-            for k in range(-half, half+1):
-                values[k+half]=spect[i+k, j]
-            spect[i, j] = median(values)
+    cdef int half = (count - 1) // 2
+
+    for j in range(n):
+        for i in range(0, half):
+            for k in range(-i, half + 1):
+                values[k + half] = spect[i + k, j]
+            spect[i, j] = median(values, half - i, count)
+        for i in range(half, m - half):
+            for k in range(-half, half + 1):
+                values[k + half] = spect[i + k, j]
+            spect[i, j] = median(values, 0, count)
+        for i in range(m - half, m):
+            for k in range(-half, m - i):
+                values[k + half] = spect[i + k, j]
+            spect[i, j] = median(values, 0, half + m - i)
 
     return spect
-
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -82,13 +91,21 @@ def horfilter(np.ndarray[DTYPE_t, ndim=2] spect, int count):
     spect = np.copy(spect)
     cdef int m = spect.shape[0]
     cdef int n = spect.shape[1]
-    cdef np.ndarray[DTYPE_t, ndim=1] values = np.zeros(count, dtype=DTYPE) # vrednosti od kojih se uzima medijana
+    cdef np.ndarray[DTYPE_t, ndim=1] values = np.zeros(count, dtype=DTYPE)  # vrednosti od kojih se uzima medijana
     cdef int i, j, k
-    cdef int half = (count-1)//2
+    cdef int half = (count - 1) // 2
     for i in range(0, m):
-        for j in range(half, n-half):
-            for k in range(-half, half+1):
-                values[k+half]=spect[i, j+k]
-            spect[i, j] = median(values)
+        for j in range(0, half):
+            for k in range(-j, half + 1):
+                values[k + half] = spect[i, j + k]
+            spect[i, j] = median(values, half - j, count)
+        for j in range(half, n - half):
+            for k in range(-half, half + 1):
+                values[k + half] = spect[i, j + k]
+            spect[i, j] = median(values, 0, count)
+        for j in range(n - half, n):
+            for k in range(-half, n - j):
+                values[k + half] = spect[i, j + k]
+            spect[i, j] = median(values, 0, half + n - j)
 
     return spect
