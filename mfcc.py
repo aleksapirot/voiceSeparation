99c654134b3d616cae110a803270330bece54398
Srđan Radović <srdjan.radovic99@gmail.com>
2017-10-08 01:05:31 +0200

    malo brzi mfcc

--- mfcc.py
+++ mfcc.py
@@ -184,8 +184,16 @@ def fbank(signal, samplerate=16000, winlen=0.025, winstep=0.01,
     energy = numpy.sum(pspec, 1)  # this stores the total energy in each frame
     energy = numpy.where(energy == 0, numpy.finfo(float).eps, energy)  # if energy is zero, we get problems with log
 
-    fb = get_filterbanks(nfilt, nfft, samplerate, lowfreq, highfreq)
-    feat = numpy.dot(pspec, fb.T)  # compute the filterbank energies
+    global cache, cache_params
+    if 'cache_params' in globals() and nfilt == cache_params[0] and nfft == cache_params[1] and samplerate == cache_params[2] and lowfreq == cache_params[3] and highfreq == cache_params[4]:
+        fb = cache
+    else:
+        fb = get_filterbanks(nfilt, nfft, samplerate, lowfreq, highfreq).T
+
+        cache = fb
+        cache_params = [nfilt, nfft, samplerate, lowfreq, highfreq]
+
+    feat = numpy.dot(pspec, fb)  # compute the filterbank energies
     feat = numpy.where(feat == 0, numpy.finfo(float).eps, feat)  # if feat is zero, we get problems with log
 
     return feat, energy
@@ -238,56 +246,6 @@ def ssc(signal, samplerate=16000, winlen=0.025, winstep=0.01,
     R = numpy.tile(numpy.linspace(1, samplerate / 2, numpy.size(pspec, 1)), (numpy.size(pspec, 0), 1))
 
     return numpy.dot(pspec * R, fb.T) / feat
-
-
-def hz2mel(hz):
-    """Convert a value in Hertz to Mels
-
-    :param hz: a value in Hz. This can also be a numpy array, conversion proceeds element-wise.
-    :returns: a value in Mels. If an array was passed in, an identical sized array is returned.
-    """
-    return 2595 * numpy.log10(1 + hz / 700.)
-
-
-def mel2hz(mel):
-    """Convert a value in Mels to Hertz
-
-    :param mel: a value in Mels. This can also be a numpy array, conversion proceeds element-wise.
-    :returns: a value in Hertz. If an array was passed in, an identical sized array is returned.
-    """
-    return 700 * (10 ** (mel / 2595.0) - 1)
-
-
-'''# TODO Cython
-def get_filterbanks(nfilt=20, nfft=512, samplerate=16000, lowfreq=0, highfreq=None):
-    """Compute a Mel-filterbank. The filters are stored in the rows, the columns correspond
-    to fft bins. The filters are returned as an array of size nfilt * (nfft/2 + 1)
-
-    :param nfilt: the number of filters in the filterbank, default 20.
-    :param nfft: the FFT size. Default is 512.
-    :param samplerate: the samplerate of the signal we are working with. Affects mel spacing.
-    :param lowfreq: lowest band edge of mel filters, default 0 Hz
-    :param highfreq: highest band edge of mel filters, default samplerate/2
-    :returns: A numpy array of size nfilt * (nfft/2 + 1) containing filterbank. Each row holds 1 filter.
-    """
-    highfreq = highfreq or samplerate / 2
-    assert highfreq <= samplerate / 2, "highfreq is greater than samplerate/2"
-
-    # compute points evenly spaced in mels
-    lowmel = hz2mel(lowfreq)
-    highmel = hz2mel(highfreq)
-    melpoints = numpy.linspace(lowmel, highmel, nfilt + 2)
-    # our points are in Hz, but we use fft bins, so we have to convert
-    #  from Hz to fft bin number
-    bin = numpy.floor((nfft + 1) * mel2hz(melpoints) / samplerate)
-
-    fbank = numpy.zeros([nfilt, nfft // 2 + 1])
-    for j in range(0, nfilt):
-        for i in range(int(bin[j]), int(bin[j + 1])):
-            fbank[j, i] = (i - bin[j]) / (bin[j + 1] - bin[j])
-        for i in range(int(bin[j + 1]), int(bin[j + 2])):
-            fbank[j, i] = (bin[j + 2] - i) / (bin[j + 2] - bin[j + 1])
-    return fbank'''
 
 
 def lifter(cepstra, L=22):
