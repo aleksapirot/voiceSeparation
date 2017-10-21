42269f7c8c70ffda21a46e51e2a27ce93a3cefac
Srđan Radović <srdjan.radovic99@gmail.com>
2017-10-21 11:29:10 +0200

    brisanje nepotrenog koda

--- scrap2.py
+++
@@ -1,6 +0,0 @@
-from plca import *
-
-# train()
-rate, audio = load('../base/MIR-1K/Wavfile/amy_5_04.wav',
-                   mono=True)  # load('../base/MIR-1K/Wavfile/amy_8_01.wav', mono=True)#load('../base/exyu/exyu_13.wav', mono=True)
-print(label(audio, rate))
